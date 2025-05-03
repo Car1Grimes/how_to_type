617cda06c645009ec4c3ea5b3271737f5e106415
thaivhm <hoangminhthaiv@gmail.com>
2025-05-03 15:47:34 +0700

    Delete js files

--- hello.js
+++
@@ -1,5 +0,0 @@
-console.log("Hello, world");
-function greet(name, date) {
-    console.log("Hello ".concat(name, ", today is ").concat(date.toDateString()));
-}
-greet("Brendan", new Date());
--- hello.ts
+++
@@ -1,7 +0,0 @@
-console.log("Hello, world");
-
-function greet(name: String, date: Date) {
-  console.log(`Hello ${name}, today is ${date.toDateString()}`);
-}
-
-greet("Brendan", new Date());
