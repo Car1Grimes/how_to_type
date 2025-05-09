1194027b26868495a2f606a024d09bedd08f1361
thaivhm <hoangminhthaiv@gmail.com>
2025-05-09 15:18:34 +0700

    Re-create react-ts project using Vite

---
+++ day_04/my-react-ts-app/src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Hello world</h1>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
