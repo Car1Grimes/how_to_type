bfe03ff089daa9007681b5ed60b014ef0ec6fed7
thaivhm <hoangminhthaiv@gmail.com>
2025-05-14 18:06:00 +0700

    experiment radix-ui, react-query

--- day_05/first-project/src/App.tsx
+++ day_05/first-project/src/App.tsx
@@ -1,19 +1,81 @@
-import { Button } from "./components/Button";
-import { useState } from "react";
+import { Theme } from "@radix-ui/themes";
+import { ThemeProvider } from "next-themes";
+import { Link, Routes, Route } from "react-router-dom";
+import { Home, About } from "./components";
+import {
+  useQuery,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 function App() {
-  const [countClick, setCountClick] = useState<number>(0);
-
-  function handleClick() {
-    setCountClick(countClick + 1);
-  }
+  const queryClient = new QueryClient();
   return (
-    <>
-      <h1>Hello world</h1>
-      <Button type="button" onClick={handleClick} label="Click" className="">
-        <label>Test click</label>
-        <p>Count clicks: {countClick}</p>
-      </Button>
-    </>
+    <ThemeProvider attribute="class">
+      <Theme>
+        <div>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+        </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+        <QueryClientProvider client={queryClient}>
+          <Example />
+          <Example_02 />
+        </QueryClientProvider>
+      </Theme>
+    </ThemeProvider>
+  );
+}
+
+function Example_02() {
+  const { isPending, error, data } = useQuery({
+    queryKey: ["cats"],
+    queryFn: async () => {
+      const res = await fetch(
+        "https://api.artic.edu/api/v1/artworks/search?q=cats",
+      );
+      return res.json();
+    },
+  });
+  if (isPending) {
+    return "Loading...";
+  }
+  if (error) {
+    return "Something wrong has happened";
+  }
+  return <div>{data.data[0].title}</div>;
+}
+
+function Example() {
+  const result = useQuery({
+    queryKey: ["repoData"],
+    queryFn: async () => {
+      const response = await fetch(
+        "https://api.github.com/repos/Car1Grimes/how_to_type",
+      );
+      return await response.json();
+    },
+  });
+
+  if (result.isPending) {
+    return "Loading...";
+  }
+
+  if (result.isError) {
+    return "Something wrong has occured";
+  }
+
+  return (
+    <div>
+      <h1>{result.data.full_name}</h1>
+      <p>{result.data.description}</p>
+      <strong>👀 {result.data.subscribers_count}</strong>{" "}
+      <strong>✨ {result.data.stargazers_count}</strong>{" "}
+      <strong>🍴 {result.data.forks_count}</strong>
+      <div>{result.isFetching ? "Updating..." : ""}</div>
+    </div>
   );
 }
 
